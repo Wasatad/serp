c160a567c446dd4f60418438e8d5af6bc217a0ec
Николай Кузнецов <n.smith@outlook.com>
2022-11-03 17:18:04 +0300

    fourth commit

---
+++ pages/policy/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h2 class="policy-title">Политика конфиденциальности</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1430px;
+  width: 100%;
+  padding: 0 15px;
+  margin: 0 auto;
+  overflow: hidden;
+  position: relative;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+}
+.policy-title {
+  margin-top: 200px;
+  margin-bottom: 900px;
+}
+</style>
